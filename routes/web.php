aca23af08f85092dfbeb9c1abc936c649bbd7aaa
nkredbear <48315340+nkredbear@users.noreply.github.com>
2020-08-25 14:44:12 +0200

    Add files via upload

---
+++ routes/web.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', 'PagesController@index');
+Route::get('/about', 'PagesController@about');
+Route::get('/services', 'PagesController@services');
+
+Route::resource('posts','PostsController');
+
+/*Route::get('/', function () {
+    return view('welcome');
+});*/
+
+/*Route::get('/about', function(){
+    return view('pages.about');
+});
+
+Route::get('/users/{id}/{name}', function($id, $name){
+    return 'This is user '.$name. ' with an id of ' .$id; 
+});
+*/
+
+Auth::routes();
+
+Route::get('/dashboard', 'DashboardController@index');
